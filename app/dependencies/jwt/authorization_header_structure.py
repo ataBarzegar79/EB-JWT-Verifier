586caf7ec38a92dfeacdde03c0c160e790033edd
Ata <atabadrbarzegar@gmail.com>
2026-09-12 18:01:45 +0300

    fix: remove the jwt package from this file to limit to general tests at this level.

---
+++ app/dependencies/jwt/authorization_header_structure.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException
+
+
+def _fail_if_header_is_none(auth_header: str | None) -> None:
+    if auth_header is None:
+        raise HTTPException(status_code=401, detail='Authorization header is missing')
+
+
+def _fail_if_header_structure_is_wrong(auth_header: str) -> None:
+    parts = auth_header.split()
+    if len(parts) != 2:
+        raise HTTPException(status_code=400, detail='Authorization header must be' + '"Bearer <token>".')
+    if parts[0].lower() != "bearer":
+        raise HTTPException(status_code=400, detail='Authorization header must start with Bearer.')
+    token_parts = parts[1].split(".")
+    if len(token_parts) != 3:
+        raise HTTPException(status_code=400, detail='JWT token is cannot be decoded. Make sure it follows the '
+                                                    'structure: header.payload.signature all in base64url encoded '
+                                                    'format.')
+    for part in token_parts:
+        if len(part) % 4 != 0 or len(part) == 0:
+            raise HTTPException(status_code=400, detail='JWT token is cannot be decoded. Header, payload or Signature '
+                                                        'is not Base64 encoded.')
+
+
+def check_authorization_header_structure(auth_header: str | None) -> None:
+    _fail_if_header_is_none(auth_header)
+    _fail_if_header_structure_is_wrong(auth_header)
--- tests/dependencies/jwt/test_auth_header_structure.py
+++ tests/dependencies/jwt/test_auth_header_structure.py
@@ -1,33 +1,42 @@
 from fastapi import HTTPException
 import pytest
 
-
-from app.dependencies.jwt.auth_header_structure import check_auth_header_structure
+from app.dependencies.jwt.authorization_header_structure import check_authorization_header_structure
 
 
 def test_missing_auth_header_fails():
     with pytest.raises(HTTPException) as exception:
-        check_auth_header_structure(auth_header=None)
+        check_authorization_header_structure(auth_header=None)
     assert exception.value.status_code == 401
     assert exception.value.detail == 'Authorization header is missing'
 
 
 def test_wrong_structure_for_header_fails():
     with pytest.raises(HTTPException) as exception:
-        check_auth_header_structure(auth_header=";ldsf,")
+        check_authorization_header_structure(auth_header=";ldsf,")
     assert exception.value.status_code == 400
     assert exception.value.detail == 'Authorization header must be' + '"Bearer <token>".'
 
 
 def test_auth_header_fails_if_not_bearer():
     with pytest.raises(HTTPException) as exception:
-        check_auth_header_structure(auth_header="another token")
+        check_authorization_header_structure(auth_header="another token")
     assert exception.value.status_code == 400
     assert exception.value.detail == 'Authorization header must start with Bearer.'
 
 
 def test_auth_header_fails_if_token_is_invalid():
     with pytest.raises(HTTPException) as exception:
-        check_auth_header_structure(auth_header="Bearer invalid_token")
-        assert exception.value.status_code == 400
-        assert exception.value.detail == 'JWT token is cannot be decoded.'
+        check_authorization_header_structure(auth_header="Bearer A.B.C")
+    assert exception.value.status_code == 400
+    assert exception.value.detail == ('JWT token is cannot be decoded. Header, payload or Signature is not Base64 '
+                                      'encoded.')
+
+
+def test_auth_header_fails_if_token_missing_parts():
+    with pytest.raises(HTTPException) as exception:
+        check_authorization_header_structure(auth_header="Bearer A.B")
+    assert exception.value.status_code == 400
+    assert exception.value.detail == ('JWT token is cannot be decoded. Make sure it follows the '
+                                      'structure: header.payload.signature all in base64url encoded '
+                                      'format.')
