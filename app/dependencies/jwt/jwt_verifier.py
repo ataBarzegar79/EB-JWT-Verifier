36e929851e022330d6fc00835095b401c790ec9c
Ata <atabadrbarzegar@gmail.com>
2026-09-13 16:07:18 +0300

    test: add test for the main jwt verifier pipeline.
    - remove several small issue while testing.

--- app/dependencies/jwt/jwt_verifier.py
+++ app/dependencies/jwt/jwt_verifier.py
@@ -1,4 +1,4 @@
-from urllib.request import Request
+from fastapi import Request
 
 from app.dependencies.jwt.public_key_against_claimed_set_verifier import verify_claimed_set_against_public_key
 from app.dependencies.jwt.x5u_url_fetcher import get_public_key_from_url
@@ -11,6 +11,6 @@ def verify_jwt(request: Request) -> None:
     auth_header = request.headers.get("Authorization")
     jwt_token = check_authorization_header_structure(auth_header=auth_header)
     decoded_tokens = check_header_and_payload(jwt_token=jwt_token)
-    parsed_url = check_x5u_validity_and_safety(x5u=decoded_tokens['header']['x5u'])
+    parsed_url = check_x5u_validity_and_safety(x5u=decoded_tokens['payload']['x5u'])
     public_key = get_public_key_from_url(url=parsed_url)
     verify_claimed_set_against_public_key(token=jwt_token, public_key=public_key)
