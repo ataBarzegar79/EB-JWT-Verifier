b53505d014579d2bde5b093692aeb24713c8b877
Ata <atabadrbarzegar@gmail.com>
2026-09-13 23:21:27 +0300

    fix: check redirect and decode content
    - response.raise_for_status does not raise if http reposne is 3XX, so a check also raises exception now.
    
    - An incoming content from a url might be gzip and this will cause issue.

--- app/dependencies/jwt/x5u.py
+++ app/dependencies/jwt/x5u.py
@@ -45,7 +45,9 @@ def _get_public_key_from_url(url: ParseResult) -> CertificatePublicKeyTypes:
                 stream=True,
             ) as response:
                 response.raise_for_status()
-                raw_content = response.raw.read()
+                if response.is_redirect:
+                    raise X5uURLNotEligibleError()
+                raw_content = response.raw.read(decode_content=True)
 
         public_key = x509.load_pem_x509_certificate(raw_content).public_key()
     except (requests.ConnectionError, requests.Timeout, FileNotFoundError, OSError):
