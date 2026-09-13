158d14d02aabb1f4a2a2b202feb4fcaf3b3abbfd
Ata <atabadrbarzegar@gmail.com>
2026-09-13 14:18:07 +0300

    fear: add the final changes to the main verifier
    - a full pipeline is complete now.
    - the process starts with general auth token verification
    - then, the claimed set is also verified before a collection of the public key.
    - x5u urls validity and security is checked
    - if all passes, the public key is downloaded and compared to the token.
    - function does not return any value for a successful attempt, but raises http exceptions for wrong operations.

--- app/dependencies/jwt/jwt_verifier.py
+++ app/dependencies/jwt/jwt_verifier.py
@@ -1,5 +1,6 @@
 from urllib.request import Request
 
+from app.dependencies.jwt.public_key_against_claimed_set_verifier import verify_claimed_set_against_public_key
 from app.dependencies.jwt.x5u_url_fetcher import get_public_key_from_url
 from app.dependencies.jwt.x5u_verifier import check_x5u_validity_and_safety
 from app.dependencies.jwt.jwt_header_and_payload_verifier import check_header_and_payload
@@ -8,8 +9,8 @@ from app.dependencies.jwt.authorization_header_structure_verifier import check_a
 
 def verify_jwt(request: Request) -> None:
     auth_header = request.headers.get("Authorization")
-    check_authorization_header_structure(auth_header=auth_header)
-    decoded_tokens = check_header_and_payload(auth_header=auth_header)
+    jwt_token = check_authorization_header_structure(auth_header=auth_header)
+    decoded_tokens = check_header_and_payload(jwt_token=jwt_token)
     parsed_url = check_x5u_validity_and_safety(x5u=decoded_tokens['header']['x5u'])
-    publicKey = get_public_key_from_url(url=parsed_url)
-
+    public_key = get_public_key_from_url(url=parsed_url)
+    verify_claimed_set_against_public_key(token=jwt_token, public_key=public_key)
